bb7e8aef319554e8ed6b77227853dde18f841494
Modi1987 <ms@uc.pt>
2019-10-03 16:31:02 +0100

    Updated, error-trap mechanism added

--- python_client/Getters.py
+++ python_client/Getters.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar 27 14:42:39 2018
-
+Updated 1-Oct-2019
+A while loop is added in the get functions. 
 @author: Mohammad SAFEEA
 """
 
@@ -11,6 +12,7 @@ class Getters:
     
     def __init__(self,mysoc):
         self.mysoc=mysoc
+        self.numOfIterations=5
 
     def send(self,data,size):
         data=data+'\n'
@@ -28,48 +30,105 @@ class Getters:
        
 # getters           
     def getEEFPos(self):
-        theCommand='Eef_pos'
-        eefPos=self.send(theCommand,6)
+        eefPos=[]
+        i=0
+        while len(eefPos)==0: # Keeps trying (up-to numOfIterations) until data is read successfully
+            theCommand='Eef_pos'
+            eefPos=self.send(theCommand,6)
+            i=i+1
+            if i==self.numOfIterations:
+                return eefPos # break
         return eefPos
         
     def getEEF_Force(self):
-        theCommand='Eef_force'
-        force=self.send(theCommand,3)
+        force=[]
+        i=0
+        while len(force)==0: # Keeps trying (up-to numOfIterations) until data is read successfully
+            theCommand='Eef_force'
+            force=self.send(theCommand,3)
+            i=i+1
+            if i==self.numOfIterations:
+                return force # return (breaks the loop)
         return force
         
     def getEEFCartizianPosition(self):
-        theCommand='Eef_pos'
-        eefPos=self.send(theCommand,3)
+        eefPos=[]
+        i=0
+        while len(eefPos)==0:
+            theCommand='Eef_pos'
+            eefPos=self.send(theCommand,3)
+            i=i+1
+            if i==self.numOfIterations:
+                return eefPos
         return eefPos
         
     def getEEF_Moment(self):
-        theCommand='Eef_moment'
-        moment=self.send(theCommand,3)
+        moment=[]
+        i=0
+        while len(moment)==0:
+            theCommand='Eef_moment'
+            moment=self.send(theCommand,3)
+            i=i+1
+            if i==self.numOfIterations:
+                return moment
         return moment
         
     def getJointsPos(self):
-        theCommand='getJointsPositions'
-        jointsPos=self.send(theCommand,7)
+        jointsPos=[]
+        i=0
+        while len(jointsPos)==0:
+            theCommand='getJointsPositions'
+            jointsPos=self.send(theCommand,7)
+            i=i+1
+            if i==self.numOfIterations:
+                return jointPos
         return jointsPos
         
     def getJointsExternalTorques(self):
-        theCommand='Torques_ext_J'
-        taw=self.send(theCommand,7)
+        taw=[]
+        i=0
+        while len(taw)==0:
+            theCommand='Torques_ext_J'
+            taw=self.send(theCommand,7)
+            i=i+1
+            if i==self.numOfIterations:
+                return taw
         return taw
     
     def getJointsMeasuredTorques(self):
-        theCommand='Torques_m_J'
-        taw=self.send(theCommand,7)
+        taw=[]
+        i=0
+        while len(taw)==0:
+            theCommand='Torques_m_J'
+            taw=self.send(theCommand,7)
+            i=i+1
+            if i==self.numOfIterations:
+                return taw            
         return taw
         
     def getMeasuredTorqueAtJoint(self,x):
-        theCommand='Torques_m_J'
-        taw=self.send(theCommand,7)
+        r=x-int(x)
+        if (x<1) or (x>7) or (not(r*r==0)):
+            print("Joint index shall be an integer from 1 to 7")
+        taw=[]
+        i=0
+        while len(taw)==0:
+            theCommand='Torques_m_J'
+            taw=self.send(theCommand,7)
+            i=i+1
+            if i==self.numOfIterations:
+                return taw[x-1]
         return taw[x-1] # array index starts from zero, joint index start from one
     
     def getEEFCartizianOrientation(self):
-        theCommand='Eef_pos'
-        eefPos=self.send(theCommand,6)
+        eefPos=[]
+        i=0
+        while len(eefPos)==0:
+            theCommand='Eef_pos'
+            eefPos=self.send(theCommand,6)
+            i=i+1
+            if i==self.numOfIterations:
+                return eefPos[3:6]       
         return eefPos[3:6]
         
         
@@ -98,4 +157,3 @@ class Getters:
         theCommand='getPin16'
         state=self.sendShort(theCommand)
         return state
-    
