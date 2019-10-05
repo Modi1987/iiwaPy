eb00bc6ecf6e7ca2fc301015f041e02336444b99
Modi1987 <ms@uc.pt>
2019-10-05 18:32:24 +0100

    Updated for testing Arcs and Relatively Defined motions.

--- python_client/PTP.py
+++ python_client/PTP.py
@@ -10,6 +10,7 @@ import StringIO
 from Senders import Senders
 from Getters import Getters
 import sys
+import time
 
 class PTP:
     
@@ -22,16 +23,21 @@ class PTP:
         data=data+'\n'
         self.mysoc.send(data)
         message=self.mysoc.receive()
-        print(message)
-        sys.stdout.flush()
+        # print(message)
+        # sys.stdout.flush()
+        time.sleep(0.05)
     
     def awaitConfirmation(self):
         message=self.mysoc.receive()
         print(message)
         sys.stdout.flush()
     
-## arcc motions
+## Arc motions
     def movePTPArc_AC(self,theta,c,k,vel):
+        #print(theta)
+        #print(c)
+        #print(k)
+        #print(vel)
         if(len(c)<>3):
             print('Error in function [movePTPArc_AC]')
             print('Center of circle should be an array of three lements')
@@ -48,21 +54,21 @@ class PTP:
             print('Error in function [movePTPArc_AC]')
             print('Relative velocity should be a scalar')
             return          
-           
+        theta_=theta[0]   
         pos=self.getter.getEEFPos()
-        x=math.pow(c[0]-pos[0],2)+math.pow(c[1]-pos[1],2)+math.pow(c[2]-pos[2],2)
-        r=math.pow(x,0.5) 
+        r_2=math.pow(c[0]-pos[0],2)+math.pow(c[1]-pos[1],2)+math.pow(c[2]-pos[2],2)
+        r=math.pow(r_2,0.5) 
         if(r==0):
             print('Error in function [movePTPArc_AC]')
             print('radius can not be zero')
             return
-        if(theta==0):
+        if(theta_==0):
             print('Error in function [movePTPArc_AC]')
             print('angle can not be zero')
             return
         #calculate unit vector
-        x=math.pow(k[0],2)+math.pow(k[1],2)+math.pow(k[2],2)
-        normK=math.pow(x,0.5)
+        r_2=math.pow(k[0],2)+math.pow(k[1],2)+math.pow(k[2],2)
+        normK=math.pow(r_2,0.5)
         if(normK==0):
             print('Error in function [movePTPArc_AC]')
             print('Norm of direction vector k shall not be zero')
@@ -70,22 +76,30 @@ class PTP:
         k[0]=k[0]/normK
         k[1]=k[1]/normK
         k[2]=k[2]/normK
+        #print('k normalized successfully')
         s=[c[0]-pos[0],c[1]-pos[1],c[2]-pos[2]]
         s[0]=-s[0]/r
         s[1]=-s[1]/r
         s[2]=-s[2]/r
         n=[(k[1]*s[2]-k[2]*s[1]),(k[2]*s[0]-k[0]*s[2]),(k[0]*s[1]-k[1]*s[0])]
-        c1=self.rotTheThing(theta/2,r,s,n,c)
-        c2=self.rotTheThing(theta,r,s,n,c)
+        #print('Performing arc motion based on c1 and c2')
+        angle=theta_/2
+        c1=self.rotTheThing(angle,r,s,n,c)
+        angle=theta_
+        c2=self.rotTheThing(angle,r,s,n,c)
+        for i in range(3,6):
+            c1.append(pos[i])
+            c2.append(pos[i])
         self.movePTPCirc1OrintationInter(c1,c2, vel)
          
-    def rotTheThing(theta,r,s,n,c):
+    def rotTheThing(self,theta,r,s,n,c):
         c1=[0,0,0]
-        cos=math.cos(theta)
-        sin=math.sin(theta)
-        c1[0]=r*cos*s[0]+r*sin*n[0]+c[0]
-        c1[1]=r*cos*s[1]+r*sin*n[1]+c[1]
-        c1[2]=r*cos*s[2]+r*sin*n[2]+c[2]
+        cos_=math.cos(theta)
+        sin_=math.sin(theta)
+        c1[0]=r*cos_*s[0]+r*sin_*n[0]+c[0]
+        c1[1]=r*cos_*s[1]+r*sin_*n[1]+c[1]
+        c1[2]=r*cos_*s[2]+r*sin_*n[2]+c[2]
+        print(c1)
         return c1
 
     def checkErrorInRelVel(self,relVel):
@@ -118,7 +132,7 @@ class PTP:
             return
         k=[0,0,1]
         pos=self.getter.getEEFPos()
-        c1=[c,pos[2]]
+        c1=[c[0],c[1],pos[2]]
         self.movePTPArc_AC(theta,c1,k,vel)
         
     def movePTPArcXZ_AC(self,theta,c,vel):
@@ -237,7 +251,7 @@ class PTP:
             buff.write('_')
             command=buff.getvalue()
             self.send(command)
-            newPos=[0,0,0,0,0,0,0]
+            newPos=[0,0,0,0,0,0]
             newPos[0]=pos[0]
             newPos[1]=pos[1]
             newPos[2]=pos[2]
--- python_client/Tutorial_PTPmotion.py
+++ python_client/Tutorial_PTPmotion.py
@@ -49,6 +49,86 @@ try:
     print(vel)
     cPos[2]=cPos[2]-50
     iiwa.movePTPLineEEF(cPos,vel)
+    # Testing PTP line relative motions
+    jPos=[0,math.pi*20/180,0,-math.pi*70/180,0,math.pi*60/180,0];
+    vRel=[0.1]
+    iiwa.movePTPJointSpace(jPos,vRel)
+    # Print current Cartesian position
+    print('Current Cartesian position')
+    print(iiwa.getEEFPos())
+    # Line motion of EEF (specified relatively in base frame) 
+    deltaX=0
+    deltaY=0
+    deltaZ=100 # (mm)
+    vel=[150] #(mm/sec)
+    pos=[deltaX,deltaY,deltaZ]
+    print('Performing Linear motion relatively defined in Base-frame')
+    iiwa.movePTPLineEefRelBase(pos,vel)
+    pos[2]=-deltaZ
+    iiwa.movePTPLineEefRelBase(pos,vel)
+    # Line motion of EEF (specified relatively in EEF frame)
+    print('Performing Linear motion relatively defined in EEF-frame')
+    pos[2]=deltaZ
+    iiwa.movePTPLineEefRelEef(pos,vel)
+    pos[2]=-deltaZ
+    iiwa.movePTPLineEefRelEef(pos,vel)
+    # Performing Arc motion
+    # Move to an initial position
+    print('Moving on Arcs')
+    print('First going to some initial configuration')
+    jPos=[0,-math.pi*10/180,0,-math.pi*100/180,math.pi*90/180,math.pi/2,0];
+    print("Moving the robot in joint space to angular position")
+    print(jPos)
+    vRel=[0.1]
+    print("With a relative velocity")
+    print(vRel[0])
+    iiwa.movePTPJointSpace(jPos,vRel)
+    # Get current position of EEF
+    print('Current Cartesian Pose')
+    f1=iiwa.getEEFPos()
+    print(f1)
+    f2=list(f1)
+    r=75
+    f1[1]=f1[1]+r
+    f1[2]=f1[2]-r
+    f1[5]=f1[5]+math.pi/8
+    f2[2]=f2[2]-2*r
+    f2[5]=f2[5] + math.pi/2
+    vel = [50]
+    iiwa.movePTPCirc1OrintationInter(f1,f2,vel)
+    # Move in joint space to another initial configuration
+    jPos=[0,math.pi*20/180,0,-math.pi*70/180,0,math.pi*90/180,0]
+    relVel=[0.15]
+    iiwa.movePTPJointSpace(jPos,relVel)
+    # Move EEF -100 mm in z direction
+    Pos=[0.0,0.0,-100.0]
+    vel = [50]
+    iiwa.movePTPLineEefRelBase(Pos,vel)
+    # Consider current position as the center of the arc
+    cen=iiwa.getEEFPos()
+    # Move EEF 50mm in X direction
+    Pos=[50.0,0.0,0.0]
+    iiwa.movePTPLineEefRelBase(Pos,vel)
+    # Store current Cartesian position in memory
+    begginingPoint=iiwa.getEEFPos()
+    # Moving in an arc, the arc is in an incliend plane
+    theta=[math.pi/2]
+    k=[1,1,1]
+    vel=[100]
+    c=[cen[0],cen[1],cen[2]]
+    print('Moving on an incliend Arc')
+    iiwa.movePTPArc_AC(theta,c,k,vel)
+    # Go back to beggining point
+    vel=[150] # velocity mm/sec
+    iiwa.movePTPLineEEF(begginingPoint,vel)
+    # Move on an Arc in the XY plange
+    # using functiom movePTPArcXY_AC
+    theta=[1.98*math.pi]
+    c=[cen[0],cen[1]]
+    vel=[150]
+    print('Moving on an incliend Arc parallel to XY plane')
+    iiwa.movePTPArcXY_AC(theta,c,vel)
+    
 except:
     print('an error happened')
     
